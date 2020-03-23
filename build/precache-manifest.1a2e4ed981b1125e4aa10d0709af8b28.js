9067c77cca801afa2f9b10d01439fdc73af16043
unknown <ironmaiden1997@live.com.mx>
2020-03-22 23:12:13 -0600

    Agregué Node_modules

---
+++ build/precache-manifest.1a2e4ed981b1125e4aa10d0709af8b28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300dc3be7e67cf1793dae8b42eed6f24",
+    "url": "/sistematt/index.html"
+  },
+  {
+    "revision": "eda7fc03dbdcd6f4d21f",
+    "url": "/sistematt/static/css/main.235d1f6f.chunk.css"
+  },
+  {
+    "revision": "20346688fe2472ae81e4",
+    "url": "/sistematt/static/js/2.c274904d.chunk.js"
+  },
+  {
+    "revision": "eda7fc03dbdcd6f4d21f",
+    "url": "/sistematt/static/js/main.2bc391db.chunk.js"
+  },
+  {
+    "revision": "bfd82d08f6c3d83ab5ee",
+    "url": "/sistematt/static/js/runtime-main.2292cf99.js"
+  },
+  {
+    "revision": "56623d5f13c087ee5c18220afded1181",
+    "url": "/sistematt/static/media/alumno.56623d5f.png"
+  },
+  {
+    "revision": "ec01f75c0fc2f90073e5533bdc53eb65",
+    "url": "/sistematt/static/media/card.ec01f75c.jpg"
+  }
+]);
